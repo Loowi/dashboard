b5e83752d710f9371d9183777b4030bbd6cd80d8
AT <at@gmail.com>
2021-12-26 23:42:59 +0100

    root

--- countrymap_frontend-main/src/pages/country-specific-statistic/constant.js
+++ countrymap_frontend-main/src/pages/country-specific-statistic/constant.js
@@ -2,5 +2,6 @@ import axios from 'axios'
 
 
 export const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/"
+    // baseURL: "http://127.0.0.1:8000/"
+    baseURL: "http://34.132.36.88:8000/"
 })
